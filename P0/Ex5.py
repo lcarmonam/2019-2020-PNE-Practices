c6790824d289826add4a0eb749f3052bd29b2b12
lcarmonam <l.carmonam.2020@alumnos.urjc.es>
2021-02-24 18:46:02 +0100

    Exercises session 5

---
+++ P0/Ex5.py
@@ -0,0 +1,11 @@
+import Seq0
+
+GENE_FOLDER = "./sequences/"
+gene_list = ["U5", "ADA", "FRAT1", "FXN"]
+
+print("-----| Exercise 5 |------")
+for gene in gene_list:
+    sequence = Seq0.seq_read_fasta(GENE_FOLDER + gene + ".txt")
+    print("Gene " + str(gene) + ":" + str(Seq0.seq_count(sequence)))
+
+
